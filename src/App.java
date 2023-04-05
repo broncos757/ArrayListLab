043a1c2607c6fa80492f82622f960093679333a9
broncos757 <51344328+broncos757@users.noreply.github.com>
2023-04-04 21:33:57 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -7,22 +7,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-//public class App extends Application {
-
-    //@Override
-    //public void start(Stage stage) {
-
-        //PaneOrganizer organizer = new PaneOrganizer();
-        //Scene scene = new Scene(organizer.getRoot(), 700, 700);
-        //stage.setScene(scene);
-        //stage.show();
-
-
-    //}
-    //public static void main(String[] argv) {
-       // launch(argv);
-  //  }
-//}
 
 
 import javafx.application.Application;
